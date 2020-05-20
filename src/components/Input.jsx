635635eeb8bf2b707f151207ad32556cbd9a6465
nick wuerz <nawuerz@gmail.com>
2020-05-19 23:33:39 -0500

    added input and button event handlers

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import { Jumbotron, Button } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Jumbotron, Button } from "react-bootstrap";
 
-const Input = () => {
-    return ( 
-        <Jumbotron >
-            <h5>Book Search</h5>
-            <br/>
-            <p>Book:</p>
-            <input />
-            <Button >
-                Search
-            </Button>
-        </Jumbotron>
-     );
+class Input extends Component {
+    state = {
+        inputVal: ""
+    }
+    
+    onInputChange = (evt) => {
+        this.setState({inputVal:evt.target.value});
+        // console.log(this.state);
+        }
+
+    handleSearch = (evt) => {
+        evt.preventDefault();
+        if (this.state.inputVal) {
+            console.log(this.state.inputVal);
+        }
+    }
+
+    render() {
+    return (
+      <Jumbotron>
+        <h5>Book Search</h5>
+        <br />
+        <p>Book:</p>
+        <input onChange={this.onInputChange}/>
+        <Button onClick={this.handleSearch}>Search</Button>
+      </Jumbotron>
+    );
+  }
 }
- 
+
 export default Input;
